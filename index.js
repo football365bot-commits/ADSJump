0a7252cf43a1ff71febc045ca5016a43a545b3bc
football365bot-commits <football365bot@gmail.com>
2026-01-15 14:39:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ window.addEventListener('resize', resize);
 // =====================
 // CONFIG
 // =====================
-const GRAVITY = -0.6;
-const BASE_JUMP_FORCE = 15;
+const GRAVITY = 0.6;
+const BASE_JUMP_FORCE = -15;
 const PLAYER_SIZE = 50;
 const PLATFORM_WIDTH = 120;
 const PLATFORM_HEIGHT = 20;
